abff7c1ecba5ae11b292e960ccb521a3ee9e4e5b
romanzhukovv <zhukov.roman89@gmail.com>
2022-06-25 17:12:28 +0300

    added Kingfisher, loaded posters into collection

--- Movies/MoviesCollection/MovieCell/MovieViewCell.swift
+++ Movies/MoviesCollection/MovieCell/MovieViewCell.swift
@@ -6,16 +6,41 @@
 //
 
 import UIKit
+import Kingfisher
 
 class MovieViewCell: UICollectionViewCell {
     static let reuseId = "MovieCell"
     
+    private let posterImageView = UIImageView()
+    
     override init(frame: CGRect) {
         super.init(frame: .zero)
-        contentView.backgroundColor = .magenta
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension MovieViewCell {
+    func configureCell(movie: Movie) {
+        guard let url = URL(string: "https://image.tmdb.org/t/p/w500" + movie.poster_path) else { return }
+        posterImageView.kf.setImage(with: url)
+        
+        posterImageView.contentMode = .scaleAspectFit
+//        posterImageView.clipsToBounds = true
+        contentView.addSubview(posterImageView)
+        addConstraints()
+    }
+    
+    private func addConstraints() {
+        posterImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            posterImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            posterImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            posterImageView.widthAnchor.constraint(equalTo: contentView.widthAnchor),
+            posterImageView.heightAnchor.constraint(equalTo: contentView.heightAnchor)
+        ])
     }
 }
--- Movies/MoviesCollection/MoviesCollectionViewController.swift
+++ Movies/MoviesCollection/MoviesCollectionViewController.swift
@@ -17,29 +17,33 @@ protocol MoviesCollectionViewOutputProtocol: AnyObject {
 }
 
 class MoviesCollectionViewController: UICollectionViewController {
-    
+    private var movies: [Movie] = [] //ALERT//
     var presenter: MoviesCollectionViewOutputProtocol! //ALERT//
     
     private let configurator: MoviesCollectionConfiguratorInputProtocol = MoviesCollectionConfigurator()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         configurator.configure(with: self)
         presenter.getMovies()
+        
         title = "Movies"
         collectionView.backgroundColor = .white
+        
         self.collectionView!.register(MovieViewCell.self, forCellWithReuseIdentifier: MovieViewCell.reuseId)
     }
 
     // MARK: UICollectionViewDataSource
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        20
+        movies.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MovieViewCell.reuseId, for: indexPath) as? MovieViewCell else { return UICollectionViewCell() }
-        
+        let movie = movies[indexPath.row]
+        cell.configureCell(movie: movie)
         return cell
     }
 }
@@ -60,6 +64,7 @@ extension MoviesCollectionViewController: UICollectionViewDelegateFlowLayout {
 
 extension MoviesCollectionViewController: MoviesCollectionViewInputProtocol {
     func sendMovies(movies: [Movie]) {
-        print(movies)
+        self.movies = movies
+        collectionView.reloadData()
     }
 }
