13a2de2f2fae9e31365f5e944213ebd2e49d2f61
romanzhukovv <zhukov.roman89@gmail.com>
2022-08-07 23:18:44 +0300

    refactoring

--- Movies/MoviesCollection/MoviesCollectionRouter.swift
+++ Movies/MoviesCollection/MoviesCollectionRouter.swift
@@ -13,6 +13,7 @@ protocol MovieCollectionRouterInputProtocol {
 }
 
 class MoviesCollectionRouter: MovieCollectionRouterInputProtocol {
+    
     unowned let viewController: MoviesCollectionViewController
     
     required init(viewController: MoviesCollectionViewController) {
